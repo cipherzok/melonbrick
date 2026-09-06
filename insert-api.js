e7fe6216616007fca33d62096a9400b639db7774
cipherzok <cipherzok@gmail.com>
2026-09-06 10:42:31 -0600

    Fix root references in export generation

--- insert-api.js
+++ insert-api.js
@@ -72,7 +72,42 @@ for (const obfuscate in fileNodes) {
     ));
 }
 
+const root = {};
+
+for (const name in deobfuscateData) {
+    const parts = name.split(".");
+    const reference = parts.pop();
+    if (parts.length === 0) {
+        if (!deobfuscateData[name].isGlobal) root[reference] = "rootReference";
+        continue;
+    } 
+    let target = root;
+    for (const part of parts) {
+        if (!target[part]) target[part] = {}
+        target = target[part];
+    }
+}
+
+function recursive(branch) {
+    const objectExpression = t.objectExpression([]);
+    for (const folder of Object.keys(branch)) {
+        const objectProperty = t.objectProperty(t.identifier(folder), recursive(branch[folder]))
+        objectExpression.properties.push(objectProperty);
+    }
+    return objectExpression;
+}
+
+for (const namespace in root) {
+    const identifier = t.identifier(namespace);
+    let init;
+    if (root[namespace] !== "rootReference") init = recursive(root[namespace]);
+    const declarator = t.variableDeclarator(identifier, init);
+    const variableDeclaration = t.variableDeclaration("var", [declarator])
+    haxeAST.program.body.push(t.exportNamedDeclaration(variableDeclaration))
+}
+
 const bundle = parser.parse("");
 bundle.program.body = Object.values(fileNodes).flat();
+fs.writeFileSync("haxe.js", generator(haxeAST).code);
 fs.writeFileSync("residue.js", generator(iifeAST).code);
 fs.writeFileSync("bundle.js", generator(bundle).code);
